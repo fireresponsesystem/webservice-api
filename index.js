9e703c7d5fd29132b23638bd4c8b611c909c783d
Romel Rey Silva <sromelrey@gmail.com>
2024-10-06 16:21:50 +0800

    working api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // REST API Server (rest-api-server.js)
 import express from "express";
 import pkg from "pg";
+import axios from "axios";
 const { Client } = pkg;
 const app = express();
 const PORT = 3001;
@@ -30,12 +31,44 @@ app.post("/api/sms", async (req, res) => {
   const house_no = String(data.house_no);
 
   try {
-    // Fetch account information and insert into database
-    // ...
+    // Fetch account information based on the house number (use parameterized query)
+    const result = await pgClient.query(
+      `SELECT coordinates, owner FROM accounts WHERE house_no = $1`,
+      [house_no]
+    );
+
+    // Check if any account was found
+    if (result.rows.length === 0) {
+      return res.status(400).json({
+        message: `There is no existing account with house id ${house_no}`,
+      });
+    }
+
+    const { coordinates, owner } = result.rows[0];
+
+    const image_url =
+      "https://www.dkiservices.com/wp-content/uploads/2020/02/Is-Food-Safe-to-Eat-After-a-Fire_.jpg";
+
+    // Insert data into the notifications table
+    await pgClient.query(
+      `INSERT INTO notifications (house_no, owner, coordinates, image_url, date_and_time_recorded) 
+         VALUES ($1, $2, $3, $4, NOW())`,
+      [house_no, owner, coordinates, image_url]
+    );
+
+    // Insert data into the notifications table
+    await pgClient.query(
+      `INSERT INTO reports (house_no, owner, coordinates, image_url, date_and_time_recorded) 
+           VALUES ($1, $2, $3, $4, NOW())`,
+      [house_no, owner, coordinates, image_url]
+    );
 
     // Send notification to WebSocket server
     const message = JSON.stringify({ house_no, coordinates, owner, image_url });
-    await axios.post("http://localhost:80/notify", { message });
+
+    await axios.post("https://websocket-server-14gk.onrender.com/notify", {
+      message,
+    });
 
     res.status(200).json({ message: "Data inserted successfully" });
   } catch (error) {
